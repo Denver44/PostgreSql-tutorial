1246371e997af06017f5bb1f2e526e27d8ce88d7
Denver <70090559+Denver44@users.noreply.github.com>
2022-01-06 22:23:20 +0530

    Creating foreign keys

---
+++ 11ForgeinKey/sql.sql
@@ -0,0 +1,31 @@
+CREATE TABLE photos(
+  id SERIAL PRIMARY KEY , 
+  url VARCHAR(200),
+  user_id INTEGER REFERENCES users(id) 
+);
+
+INSERT INTO
+  photos(url, user_id)
+VALUES
+  ('http://img1.jpg', 3),
+  ('http://img2.jpg', 1),
+  ('http://img3.jpg', 2),
+  ('http://img4.jpg', 3);
+
+
+SELECT * FROM  photos
+
+id	url	        user_id
+1	http://img1.jpg	3
+2	http://img2.jpg	1
+3	http://img3.jpg	2
+4	http://img4.jpg	3
+
+
+
+-- MYSQL SYNATX FOR FOREIGN KEY
+-- CREATE TABLE photoes (
+--     id int NOT NULL PRIMARY KEY,
+--     url VARCHAR(200),
+--     user_id int FOREIGN KEY REFERENCES users(id)
+-- );
