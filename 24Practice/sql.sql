f41e86ac58d1f4beb320f31882385c949fb2f51b
Denver <70090559+Denver44@users.noreply.github.com>
2022-01-08 23:13:06 +0530

    Union, intersects , having groupby and orderby done

--- 24Practice/sql.sql
+++ 24Practice/sql.sql
@@ -96,3 +96,24 @@ GROUP BY
 HAVING
   SUM(price * units_sold) > 2000000
 
+
+
+(
+  SELECT
+    manufacturer
+  FROM
+    phones
+  WHERE
+    price < 170
+)
+UNION
+  (
+    SELECT
+      manufacturer
+    FROM
+      phones
+    GROUP BY
+      manufacturer
+    HAVING
+      count(*) > 2
+  )
