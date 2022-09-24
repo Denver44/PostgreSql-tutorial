9449c4515610c0702c743594a9285faacaa22992
Denver <70090559+Denver44@users.noreply.github.com>
2022-09-24 18:07:52 +0530

    SQL

---
+++ 1SQL_Basics/1CreateTable/sql.sql
@@ -0,0 +1,13 @@
+
+-- KEYWORDS :-  1. CREATE   2. TABLE
+-- CREATE Keyword helps us to craete a table and here we have created a Table name cities 
+-- To create columns for our Table we have to name the coulumn in open and close brackets ()
+-- VARCHAR (50) means it will contains string whose lenght will be 50.
+
+
+CREATE TABLE cities (
+  name VARCHAR(50),
+  country VARCHAR(50),
+  population INTEGER,
+  area INTEGER
+);
